6b2aa31c71b3c013847c71cad196a1319f73ada9
lobotskiy <lobotskiy09@gmail.com>
2021-03-01 17:56:52 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 import Music from './components/Music/Music';
@@ -9,25 +8,31 @@ import { Route } from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
-const App = (props) => {
-
+const App = () => {
   return (
     <div className='app-wrapper'>
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className='app-wrapper-content'>
-        <Route path='/profile' render={() => <ProfileContainer />} />
+        <Route path='/profile/:userId?'
+          render={() => <ProfileContainer />} />
 
-        <Route path='/dialogs' render={() => <DialogsContainer />} />
+        <Route path='/dialogs'
+          render={() => <DialogsContainer />} />
 
-        <Route path='/news' render={() => <News />} />
+        <Route path='/news'
+          render={() => <News />} />
 
-        <Route path='/music' render={() => <Music />} />
+        <Route path='/music'
+          render={() => <Music />} />
 
-        <Route path='/settings' render={() => <Settings />} />
+        <Route path='/settings'
+          render={() => <Settings />} />
 
-        <Route path='/users' render={() => <UsersContainer />} />
+        <Route path='/users'
+          render={() => <UsersContainer />} />
       </div>
     </div>
   )
